78306c53ef1affe604288dddef3076d23da3f59b
Ashish Singh <ashishcodes4@gmail.com>
2018-11-28 05:43:36 +0530

    deploy state

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     this.connecToServer = this.connecToServer.bind(this);
   }
   connecToServer() {
-    fetch('/');
+    fetch('/')
   }
 
   componentDidMount() {
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
+const exp = 400;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
 app.listen(port, (req, res) => {
