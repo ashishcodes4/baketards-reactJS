b15d1ad4d3668f230f01ef3c7f49b0fb874dde25
Ashish Singh <ashishcodes4@gmail.com>
2018-11-28 06:23:45 +0530

    deploy v7

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const exp = 400;
 
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'))
+  })
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
